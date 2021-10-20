3707d0d1ebae0b70dc53d3cf6569ad20683a15c6
sominlee92 <sominlee92@gmail.com>
2021-10-20 12:09:35 -0700

    map image upload
    map image upload

--- lee.somin/js/app.js
+++ lee.somin/js/app.js
@@ -28,19 +28,21 @@ $(()=>{
 
 
    .on("click","[data-activate]",function(e){
-      e.preventDefault();
       let target = $(this).data("activate");
       $(target).addClass("active");
    })
    .on("click","[data-deactivate]",function(e){
-      e.preventDefault();
       let target = $(this).data("deactivate");
       $(target).removeClass("active");
    })
    .on("click","[data-toggle]",function(e){
-      e.preventDefault();
       let target = $(this).data("toggle");
       $(target).toggleClass("active");
+   })
+   .on("click","[data-activateone]",function(e){
+      let target = $(this).data("activateone");
+      console.log(target)
+      $(target).addClass("active").siblings().removeClass('active');
    })
    ;
 
@@ -50,6 +52,11 @@ $(()=>{
       $(this).html($(target).html());
    })
 
+   $({
+      "#page-recent":".nav-icon-set li:nth-child(1)",
+      "#page-list":".nav-icon-set li:nth-child(2)",
+      "#page-user-profile":".nav-icon-set li:nth-child(3)",
+   }[location.hash]).addClass("active");
 
 
 });
